fe5bf7c90e3999f1695e1c01fa60d55a6cc7347c
Arif Mainuddin <arifmainuddin18@gmail.com>
2025-01-16 14:57:35 +0600

    module-45_4

---
+++ Module-45/module-45_4/src/Components/About/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <div>
+            <h1 style={{color:"whitesmoke"}}>This is about page..!</h1>
+        </div>
+    );
+};
+
+export default About;
---
+++ Module-45/module-45_4/src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div>
+            <h2 style={{color:'red'}}>This is footer..!</h2>
+        </div>
+    );
+};
+
+export default Footer;
